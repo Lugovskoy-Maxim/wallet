d320544bf10986a8132638fe7f5aa15dac36c296
Максим Луговской <lugovskou.myu@yandex.ru>
2024-12-04 23:23:51 +0500

    fix: remove p element

--- app/page.tsx
+++ app/page.tsx
@@ -39,21 +39,21 @@ export default function Overview() {
         <div className={styles.wallet}>
           <h2>Баланс</h2>
           <div>
-            <p>₽</p>
-            <p>35,502.45</p>
+
+            <p>₽35,502.45</p>
             <span>12,5%</span>
           </div>
         </div>
         <div className={styles.income}>
           <h2>Доходы</h2>
           <div>
-            <p>₽</p><p>49,450.00</p><span>27%</span>
+            <p>₽49,450.00</p><span>27%</span>
           </div>
         </div>
         <div className={styles.expense}>
           <h2>Расходы</h2>
           <div>
-            <p>₽</p><p>13,947.55</p><span>-15%</span>
+            <p>₽13,947.55</p><span>-15%</span>
           </div>
         </div>
       </div>
