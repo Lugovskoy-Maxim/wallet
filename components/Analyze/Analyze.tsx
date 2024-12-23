c0e31e2a788d612eb585e7ff82e6e16a4af4855f
Максим Луговской <lugovskou.myu@yandex.ru>
2024-12-23 16:50:02 +0500

    up adaptive components

--- components/Analyze/Analyze.tsx
+++ components/Analyze/Analyze.tsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 import styles from "./analyze.module.scss";
 import ExamplePieChart from "../PieChart/PieChart";
 import LastTransactions from "../LastTransactions/LastTransactions";
-import { analysisData  } from "@/constants/MokeData";
+import { analysisData } from "@/constants/MokeData";
 
 interface DataType {
   id?: number;
@@ -14,29 +14,31 @@ interface DataType {
 
 export default function Analyze() {
   // заменить на данные из state rtk
-  const [data] = useState<DataType[]>(analysisData );
+  const [data] = useState<DataType[]>(analysisData);
 
   return (
     <section className={styles.analyze}>
       <div className={styles.wheelGraph}>
         <h2>Расходы по категориям</h2>
-        <div className={styles.wheel}>
-          <ExamplePieChart data={data} />
+        <div className={styles.wrapper}>
+          <div className={styles.wheel}>
+            <ExamplePieChart data={data} />
+          </div>
+          <ul>
+            {data.map((item) => (
+              <li key={item.name}>
+                <span
+                  className={styles.color}
+                  style={{ backgroundColor: item.color }}
+                ></span>
+                <div className={styles.info}>
+                  <span className={styles.name}>{item.name}</span>
+                  <span className={styles.value}>{item.value} ₽</span>
+                </div>
+              </li>
+            ))}
+          </ul>
         </div>
-        <ul>
-          {data.map((item) => (
-            <li key={item.name}>
-              <span
-                className={styles.color}
-                style={{ backgroundColor: item.color }}
-              ></span>
-              <div className={styles.info}>
-                <span className={styles.name}>{item.name}</span>
-                <span className={styles.value}>{item.value} ₽</span>
-              </div>
-            </li>
-          ))}
-        </ul>
       </div>
       <LastTransactions />
     </section>
