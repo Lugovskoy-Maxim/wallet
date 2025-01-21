a778106486c70cd2ac326726359366a148a98a5e
Максим Луговской <lugovskou.myu@yandex.ru>
2025-01-21 16:35:39 +0500

    refactor transactions table

--- components/LastTransactions/LastTransactions.tsx
+++ components/LastTransactions/LastTransactions.tsx
@@ -37,38 +37,40 @@ export default function LastTransactions() {
         <h2>Последние транзакции</h2>
         <p>Проверьте свои последние транзакции</p>
       </div>
-      <div className={styles.thead}>
-        <span className={styles.color}></span>
-        <p className={styles.name}>Название</p>
-        <p className={styles.date}>Дата</p>
-        <p className={styles.category}>Категория</p>
-        <p className={styles.amount}>Сумма</p>
-        <p className={styles.action}></p>
-      </div>
-      <ul className={styles.list}>
-        {transactions.map(
-          ({ id, name, date, type, amount: amount, category }) => (
-            <li key={id}>
-              <span
+      <table className={styles.table}>
+        <thead className={styles.thead}>
+          <tr>
+            <th className={styles.color}></th>
+            <th className={styles.name}>Название</th>
+            <th className={styles.date}>Дата</th>
+            <th className={styles.category}>Категория</th>
+            <th className={styles.amount}>Сумма</th>
+            <th className={styles.action}></th>
+          </tr>
+        </thead>
+        <tbody className={styles.list}>
+          {transactions.map(({ id, name, date, type, amount, category }) => (
+            <tr key={id}>
+              <td
                 className={styles.color}
                 style={{ backgroundColor: category.color }}
-              ></span>
-              <p className={styles.name}>{name}</p>
-              <p className={styles.name}>{name}</p>
-              <p className={styles.date}>{date}</p>
-              <p className={styles.category}>{category.name}</p>
-              <p className={`${styles.amount} ${styles[type]}`}>
+              ></td>
+              <td className={styles.name}>{name}</td>
+              <td className={styles.date}>{date}</td>
+              <td className={styles.category}>{category.name}</td>
+              <td className={`${styles.amount} ${styles[type]}`}>
                 {geAmount(type, amount)}
-              </p>
-              <p className={styles.action}>
+              </td>
+              <td className={styles.action}>
                 <button type="button">
-                  <Image src={action_icon} alt={"Опции"} />
+                  <Image src={action_icon} alt="Опции" />
                 </button>
-              </p>
-            </li>
-          )
-        )}
-      </ul>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+
       {<button className={styles.loadMore}>Загрузить еще</button>}
     </div>
   );
